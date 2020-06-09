8753da6979984a77f0342e003377c08be318e9e7
Md Azizul Hakim <ahslucky12@gmail.com>
2020-06-10 02:33:06 +0600

    invoice print operation

--- resources/views/invoice/invoiceDetailsView.blade.php
+++ resources/views/invoice/invoiceDetailsView.blade.php
@@ -3,8 +3,8 @@
 <section class="invoice-view-wrapper">
     <div class="row">
       <!-- invoice view page -->
-      <div class="col-xl-9 col-md-8 col-12">
-        <div class="card">
+      <div id="invoiceDiv" class="col-xl-9 col-md-8 col-12">
+        <div class="card invoice-print-area">
           <div class="card-content">
             <div class="card-body pb-0 mx-25">
               <!-- header section -->
@@ -137,7 +137,7 @@
                 </button>
               </div>
             <div class="invoice-action-btn">
-              <button class="btn btn-primary btn-block">
+              <button class="btn btn-primary btn-block invoice-print">
                 <i class='bx bx-printer'></i>
                 <span>Print Invoice</span>
               </button>
@@ -159,4 +159,90 @@
       </div>
     </div>
   </section>
+
+
+  <script>
+    //  function PrintElem(elem) {
+    //   Popup(jQuery(elem).html());
+    //   }
+
+
+
+    //   function Popup(data) {
+    
+    //   var mywindow = window.open('', 'Report', 'height=768,width=1366');
+    //     mywindow.document.write('<html><head><title></title>');
+    //     mywindow.document.write('<link rel="shortcut icon" type="image/x-icon" href="{{asset('favicon.ico')}}">');
+    //     mywindow.document.write('<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,600%7CIBM+Plex+Sans:300,400,500,600,700" rel="stylesheet">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/vendors.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/fonts/fontawesome/css/all.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/fonts/boxicons/css/boxicons.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/tables/datatable/datatables.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/ui/prism.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/file-uploaders/dropzone.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/select/select2.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/pickers/pickadate/pickadate.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/pickers/daterange/daterangepicker.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/spinner/jquery.bootstrap-touchspin.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/bootstrap.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/bootstrap-extended.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/colors.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/components.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/themes/dark-layout.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/themes/semi-dark-layout.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/menu/menu-types/vertical-menu.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/plugins/file-uploaders/dropzone.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('packages/bootstrap-sweetalert/dist/sweetalert.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('packages/sweetalert2/dist/sweetalert2.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/pages/app-invoice.min.css')}}">');
+    //     mywindow.document.write('<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/vendors.min.js')}}"><\/script>');
+    //     mywindow.document.write('</head><body>');
+       
+    //     mywindow.document.write(data);
+        
+    //     mywindow.document.write('<script src="{{asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/datatables.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/dataTables.bootstrap4.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/buttons.html5.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/buttons.print.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/pdfmake.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/tables/datatable/vfs_fonts.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/forms/validation/jqBootstrapValidation.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/extensions/dropzone.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/ui/prism.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('packages/bootstrap-sweetalert/dist/sweetalert.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/extensions/polyfill.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/forms/select/select2.full.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/pickers/pickadate/picker.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/pickers/pickadate/picker.date.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/pickers/pickadate/picker.time.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/pickers/pickadate/legacy.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/pickers/daterange/moment.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/pickers/daterange/daterangepicker.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/forms/spinner/jquery.bootstrap-touchspin.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/vendors/js/forms/repeater/jquery.repeater.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/configs/vertical-menu-light.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/core/app-menu.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/core/app.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/components.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/footer.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/customizer.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/datatables/datatable.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/modal/components-modal.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/forms/select/form-select2.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/forms/number-input.min.js')}}"><\/script>');
+    //     mywindow.document.write('<script src="{{asset('app-assets/js/scripts/pages/app-invoice.min.js')}}"><\/script>');
+    //     mywindow.document.write('</body></html>');
+    //   mywindow.document.close();
+    //   setTimeout(function () {
+    //   mywindow.print();
+    //   }, 200)
+
+    //   }
+  </script>
 @endsection
